4b855448001b522580c3e13e327117d4c44562fe
austincmatteson <austin.c.matteson@gmail.com>
2018-04-16 10:41:38 -0700

    export

--- opportune/__init__.py
+++ opportune/__init__.py
@@ -1,9 +1,11 @@
+import os
 from pyramid.config import Configurator
 
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
+    settings['sqlalchemy.url'] = os.environ.get('DATABASE_URL')
     config = Configurator(settings=settings)
     config.include('pyramid_jinja2')
     config.include('.models')
