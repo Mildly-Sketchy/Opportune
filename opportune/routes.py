602bd6f20228d848290ef96abab65ae246cb9774
Kat Cosgrove <kat.cosgrove@gmail.com>
2018-04-17 14:58:22 -0700

    keywords display on edit profile page with ability to delete

--- opportune/routes.py
+++ opportune/routes.py
@@ -4,6 +4,7 @@ def includeme(config):
     config.add_route('auth', '/auth')
     config.add_route('logout', '/logout')
     config.add_route('profile', '/profile')
+    config.add_route('profile/delete', '/profile/delete')
     config.add_route('analytics', '/analytics')
     config.add_route('about', '/about')
     config.add_route('search', '/search')
--- opportune/views/default.py
+++ opportune/views/default.py
@@ -21,7 +21,41 @@ def home_view(request):
 @view_config(route_name='profile', renderer='../templates/profile.jinja2')
 def profile_view(request):
     """Return profile settings page."""
-    return {}
+    if request.method == 'GET':
+            try:
+                query = request.dbsession.query(Keyword)
+                user_keywords = query.filter(Association.user_id == request.authenticated_userid, Association.keyword_id == Keyword.keyword)
+            except DBAPIError:
+                raise DBAPIError(DB_ERR_MSG, content_type='text/plain', status=500)
+
+            keywords = [keyword.keyword for keyword in user_keywords]
+            if len(keywords) < 1:
+                return{'message': 'You do not have any keywords saved. Add one!'}
+
+            return{'keywords': user_keywords}
+
+
+@view_config(route_name='profile/delete', renderer='../templates/profile.jinja2')
+def delete_keyword_profile(request):
+    """Delete a requested keyword from association table for that particular user."""
+    if request.method == 'POST':
+        try:
+            keyword = request.POST['keyword']
+            user = request.authenticated_userid
+        except KeyError:
+            return HTTPBadRequest()
+
+        try:
+
+            query = request.dbsession.query(Association)
+            removed = query.filter(Association.keyword_id == keyword, Association.user_id == user).one()
+
+            request.dbsession.delete(removed)
+
+            return HTTPFound(location=request.route_url('profile'))
+
+        except DBAPIError:
+            return Response(DB_ERR_MSG, content_type='text/plain', status=500)
 
 
 @view_config(route_name='analytics', renderer='../templates/admin.jinja2')
@@ -46,7 +80,8 @@ def search_view(request):
             except DBAPIError:
                 raise DBAPIError(DB_ERR_MSG, content_type='text/plain', status=500)
 
-            if len(user_keywords) < 1:
+            keywords = [keyword.keyword for keyword in user_keywords]
+            if len(keywords) < 1:
                 return{'message': 'You do not have any keywords saved. Add one!'}
 
             return{'keywords': user_keywords}
