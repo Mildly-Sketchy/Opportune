39ec2b746f4143433d65ac974beb73b34e426ae2
Kat Cosgrove <kat.cosgrove@gmail.com>
2018-04-18 16:23:43 -0700

    fixing files to use environment variable for database url

--- opportune/__init__.py
+++ opportune/__init__.py
@@ -5,7 +5,8 @@ from pyramid.config import Configurator
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
-    settings['sqlalchemy.url'] = 'postgres://localhost:5432/opportune_dev'
+    if os.environ.get('DATABASE_URL', ''):
+        settings["sqlalchemy.url"] = os.environ["DATABASE_URL"]
     config = Configurator(settings=settings)
     config.include('pyramid_jinja2')
     config.include('.models')
