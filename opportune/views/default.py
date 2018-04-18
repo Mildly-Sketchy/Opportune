bd10b713af1ddb28c701b069947b967b9fb877f4
Kat Cosgrove <kat.cosgrove@gmail.com>
2018-04-18 14:21:30 -0700

    message now displays when username is already in use

--- opportune/views/default.py
+++ opportune/views/default.py
@@ -1,9 +1,7 @@
 from pyramid.view import view_config
 from pyramid.security import NO_PERMISSION_REQUIRED
-from pyramid.response import Response
 from ..models import Keyword
 from ..models import Association
-from . import DB_ERR_MSG
 
 
 @view_config(route_name='home', renderer='../templates/index.jinja2',
