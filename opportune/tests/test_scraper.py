0e0dffd6c3d605a9b9a7512b91fca3a74d7daed0
Patricia G Raftery <praftery8@gmail.com>
2018-04-18 13:04:53 -0700

    new branch to have the bug fixes from dev branch

--- opportune/tests/test_scraper.py
+++ opportune/tests/test_scraper.py
@@ -28,17 +28,17 @@ def sub_requests_get(url):
     return DummyResponse()
 
 
-# def test_default_behavior_of_scraper(dummy_request, monkeypatch):
-#     """Test default scraper behavior."""
-#     from ..views.scraper import get_jobs
-#     from urllib3 import PoolManager
-#     # Patching the request to not actually hit the API
-#     monkeypatch.setattr(PoolManager, 'request', substitute_urllib_get_request)
-#     # import requests
-#     # monkeypatch.setattr(requests, 'get', sub_requests_get)
+def test_default_behavior_of_scraper(dummy_request, monkeypatch):
+    """Test default scraper behavior."""
+    from ..views.scraper import get_jobs
+    from urllib3 import PoolManager
+    # Patching the request to not actually hit the API
+    monkeypatch.setattr(PoolManager, 'request', substitute_urllib_get_request)
+    # import requests
+    # monkeypatch.setattr(requests, 'get', sub_requests_get)
 
-#     dummy_request.method = 'POST'
-#     dummy_request.POST = {'city': 'seattle', 'keyword': 'python'}
-#     response = get_jobs(dummy_request)
-#     # assert len(response) == 0
-#     assert type(response) == dict
+    dummy_request.method = 'POST'
+    dummy_request.POST = {'city': 'seattle', 'keyword': 'python'}
+    response = get_jobs(dummy_request)
+    # assert len(response) == 0
+    assert type(response) == dict
