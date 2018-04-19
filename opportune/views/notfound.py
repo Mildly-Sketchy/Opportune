f00f9ed7d72cef48bed3c2d8f54133e0ec4f4c5b
Kat Cosgrove <kat.cosgrove@gmail.com>
2018-04-18 21:04:11 -0700

    401 redirect for bad login caught, scraper now returns job summaries

--- opportune/views/notfound.py
+++ opportune/views/notfound.py
@@ -1,7 +1,15 @@
-from pyramid.view import notfound_view_config
+from pyramid.view import notfound_view_config, forbidden_view_config
+from pyramid.httpexceptions import HTTPFound
 
 
 @notfound_view_config(renderer='../templates/404.jinja2')
 def notfound_view(request):
+    """Handle 404 response."""
     request.response.status = 404
     return {}
+
+
+@forbidden_view_config()
+def forbidden_view(request):
+    """Handle 401 response."""
+    return HTTPFound(location=request.route_url('auth'))
